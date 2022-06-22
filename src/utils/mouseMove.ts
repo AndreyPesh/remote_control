ce96b1b4e85ee84794cea1982d731f39c0de4bff
AndreyPesh <coursestud@mail.ru>
2022-06-22 21:25:27 +0300

    refator: add mouse move coordinate

--- src/utils/mouseMove.ts
+++ src/utils/mouseMove.ts
@@ -1,21 +1,22 @@
 import robot from 'robotjs';
+import { SHIFT_MOUSE } from '../constant/number';
 
 export const mouse_up = (coordY: number) => {
   const { x, y } = robot.getMousePos();
-  robot.moveMouseSmooth(x, y - coordY);
+  robot.moveMouseSmooth(x, y - coordY - SHIFT_MOUSE);
 };
 
 export const mouse_down = (coordY: number) => {
   const { x, y } = robot.getMousePos();
-  robot.moveMouseSmooth(x, y + coordY);
+  robot.moveMouseSmooth(x, y + coordY + SHIFT_MOUSE);
 };
 
 export const mouse_left = (coordX: number) => {
   const { x, y } = robot.getMousePos();
-  robot.moveMouseSmooth(x - coordX, y);
+  robot.moveMouseSmooth(x - coordX - SHIFT_MOUSE, y);
 };
 
 export const mouse_right = (coordX: number) => {
   const { x, y } = robot.getMousePos();
-  robot.moveMouseSmooth(x + coordX, y);
+  robot.moveMouseSmooth(x + coordX + SHIFT_MOUSE, y);
 };
