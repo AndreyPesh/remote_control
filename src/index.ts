e32b3a253a441226484111ab5faadc4eb2d2716a
Andrey Peshko <coursestud@mail.ru>
2022-06-24 01:13:10 +0300

    refactor: add env file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { httpServer } from './http_server/server';
 import robot from 'robotjs';
 import WebSocket, { WebSocketServer, createWebSocketStream } from 'ws';
@@ -7,14 +8,14 @@ import { INVALID_COMMAND, SOCKET_CLOSE } from './constant/message';
 import { drawCircle, drawRectangle, drawSquare } from './utils/draw';
 import { getScreenShot } from './utils/picture';
 
-const HTTP_PORT = 3000;
-const SOCKET_PORT = 8080;
-const socketServer = new WebSocketServer({ port: SOCKET_PORT });
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const SOCKET_PORT = process.env.SOCKET_PORT || 8080;
+const socketServer = new WebSocketServer({ port: Number(SOCKET_PORT) });
 
 const clients: Set<WebSocket> = new Set();
 
 socketServer.on('connection', (ws) => {
-  console.log(`Start websocket on the ${SOCKET_PORT} port!`);
+  console.log(`Client connected on ${SOCKET_PORT} port!`);
   clients.add(ws);
   const duplex = createWebSocketStream(ws, { decodeStrings: false });
 
@@ -88,4 +89,5 @@ process.on('SIGINT', () => {
 
 httpServer.listen(HTTP_PORT, () => {
   console.log(`Start static http server on the ${HTTP_PORT} port!`);
+  console.log(`Websocket server available on the ${SOCKET_PORT} port!`);
 });
